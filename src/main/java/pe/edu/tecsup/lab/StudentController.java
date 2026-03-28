e78fd3e05cb885805655908ad59046255d10f67f
Leo <leo.pinto@tecsup.edu.pe>
2026-03-28 13:31:57 -0500

    feat: agrega lógica al método getStudents

---
+++ src/main/java/pe/edu/tecsup/lab/StudentController.java
@@ -0,0 +1,8 @@
+package pe.edu.tecsup.lab;
+
+public class StudentController {
+    public void getStudents() {
+        // TODO: agregar lógica
+        System.out.println("Mostrando lista de estudiantes...");
+    }
+}
