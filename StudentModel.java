85690ea2a3abed9e026037e9326d086f99ea09c4
diegohuacca-hub <diegohuacca@tecsup.edu.pe>
2026-03-28 13:21:07 -0500

    feat: se agrega StudentModel

---
+++ StudentModel.java
@@ -0,0 +1,23 @@
+public class StudentModel {
+    private int id;
+    private String nombre;
+    private String apellido;
+
+    public StudentModel(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+}
