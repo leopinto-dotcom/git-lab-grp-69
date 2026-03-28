1f3738852cecb56955875ab136d6d3ea954985b8
diegohuacca-hub <diegohuacca@tecsup.edu.pe>
2026-03-28 16:07:38 -0500

    feat(sprint-1): implementación de StudentModel

--- StudentModel.java
+++ StudentModel.java
@@ -3,12 +3,18 @@ public class StudentModel {
     private String nombre;
     private String apellido;
 
+    // Constructor vacío
+    public StudentModel() {
+    }
+
+    // Constructor con parámetros
     public StudentModel(int id, String nombre, String apellido) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
     }
 
+    // Getters
     public int getId() {
         return id;
     }
@@ -19,5 +25,28 @@ public class StudentModel {
 
     public String getApellido() {
         return apellido;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    // Método toString (útil para imprimir datos)
+    @Override
+    public String toString() {
+        return "StudentModel{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                '}';
     }
 }
